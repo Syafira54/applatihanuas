c2552213e245e07dbdbafb936e6a0a7607c5dcc0
Syafira54 <syafiradifa645@gmail.com>
2023-07-06 12:17:32 +0700

    UPDATE

--- application/views/update.blade.php
+++ application/views/update.blade.php
@@ -1,8 +1,8 @@
 
 <!doctype html>
+
 <html lang="en" data-bs-theme="auto">
   <head><script src="/docs/5.3/assets/js/color-modes.js"></script>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -11,9 +11,7 @@
     <title>Starter Template · Bootstrap v5.3</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/starter-template/">
-
 <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Favicons -->
 <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
 <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
@@ -28,27 +26,61 @@
   <main>
     <h1 class="text-body-emphasis">Ujian Pemrograman Web</h1>
     <hr class="col-3 col-md-2 mb-5">
-
     <div class="row g-5">
       <div class="col-md-6">
-        <h2 class="text-body-emphasis">Form Input Artikel</h2>
+        <h2 class="text-body-emphasis">Form Update Artikel</h2>
         <p>Masukan data yang akan di proses</p>
-        <form method="post" action="{{ site_url('Welcome/update/'. $post->id) }}">
+        <form method="post" action="{{ site_url('Welcome/update/' . $post->id) }}">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Username</label>
-                <select class="form-control" name="username">
-                  @foreach($avail_user as $user)
-                  <option value="{{ $user->id }}" {{ $post->user_id == $user->id ? "selected" : "" }}>{{ $user->username }}</option>
+                <select class = "form-control" name="user_id" id="">
+                  @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->username }}</option>
                   @endforeach
                 </select>
             </div>
+
+            <p>Jenis</p>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Berita" required>
+              @if($jenis == 0)
+                <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Berita" required checked>
+              @else
+                <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Berita" required>
+              @endif
+              <label class="form-check-label" for="flexRadioDefault1">
+                Berita
+              </label>
+            </div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Tutorial" required>
+              @if($jenis == 1)
+                <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Tutorial" required checked>
+              @else
+                <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Tutorial" required>
+              @endif
+              <label class="form-check-label" for="flexRadioDefault1">
+                Tutorial
+              </label>
+            </div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Blog" required>
+              @if($jenis == 2)
+                <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Blog" required checked>
+              @else
+                <input class="form-check-input" type="radio" name="radio" id="flexRadioDefault1" value="Blog" required>
+              @endif
+              <label class="form-check-label" for="flexRadioDefault1">
+                Blog
+              </label>
+            </div>
+            <br>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Artikel</label>
-                <textarea class="form-control" id="nama" name="artikel" rows="3">{{ $post->artikel }}</textarea>
+                <textarea class="form-control" id="nama" name="article" rows="3" required>{{ $post->article }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
-            <a class="btn btn-secondary" href="{{site_url('Welcome/tampil')}}">Tampil</a>
-        </form>
+          </form>
       </div>
     </div>
   </main>
